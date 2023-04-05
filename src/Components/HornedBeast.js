62ad399555fc46dec99a3bacbe19cec5968823c9
simpsonpaisley <simpsonpaisley@gmail.com>
2023-04-05 20:36:31 +0100

    Fixed Main.js, and added voting.

--- src/Components/HornedBeast.js
+++ src/Components/HornedBeast.js
@@ -1,12 +1,22 @@
+import { useState } from 'react';
+
 function HornedBeast({ title, description, imageURL }) {
+	const [votes, addVote] = useState(0);
+
+	function voteHandler() {
+		addVote(votes + 1);
+	}
 	return (
-		<div className="hornedBeast">
+		<div
+			className="hornedBeast"
+			onClick={voteHandler}>
 			<h2>{title}</h2>
 			<img
 				src={imageURL}
 				alt={title}
 				title={title}></img>
 			<p>{description}</p>
+			<h3>Votes: {votes}</h3>
 		</div>
 	);
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -33,15 +33,22 @@ const beastArray = [
 ];
 
 function Main() {
-	return beastArray.map(({ title, description, image_url }) => (
-		<>
-			<HornedBeast
-				imageURL={image_url}
-				title={title}
-				description={description}
-			/>
-		</>
-	));
+	return (
+		<div className="main">
+			<h1>Horned Beasts</h1>
+			<div className="beastDisplay">
+				{beastArray.map(
+					({ title, description, image_url, _id, keyword, horns }) => (
+						<HornedBeast
+							title={title}
+							imageURL={image_url}
+							description={description}
+						/>
+					)
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Main;
